7e65c0f1dc0bf93a7de98ad0075ee2d38eca65d6
Taku210 <87713046+Taku210@users.noreply.github.com>
2021-09-07 23:28:27 +0100

    Add files via upload

---
+++ JavaHLT3/src/JavaHLTpt3.java
@@ -0,0 +1,18 @@
+public class JavaHLTpt3 {
+   public static int max(int numA, int numB) {
+       int result;
+       if (numA > numB) {
+           result = numA;
+       } else
+       {
+           result = numB;
+       }
+       return result;
+   }
+
+    public static void main(String[] args) {
+        int a=15, b=8;
+        int c=max(a,b);
+        System.out.println("Largest value="+c);
+    }
+}
